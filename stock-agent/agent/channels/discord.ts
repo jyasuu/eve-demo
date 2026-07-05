5ac57dbc40bfcc259d6eb5c4a5c3d061288ae2f7
Leo Yen <127848825+LYee0n@users.noreply.github.com>
2026-07-05 02:19:52 +0000

    discord auth

--- stock-agent/agent/channels/discord.ts
+++ stock-agent/agent/channels/discord.ts
@@ -1,3 +1,9 @@
 import { discordChannel } from "eve/channels/discord";
 
-export default discordChannel();
+export default discordChannel({
+  credentials: {
+    botToken: process.env.DISCORD_BOT_TOKEN,
+    publicKey: process.env.DISCORD_PUBLIC_KEY,
+    applicationId: process.env.DISCORD_APPLICATION_ID,
+  },
+});
